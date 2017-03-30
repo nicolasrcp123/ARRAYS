750bd1f48fe3f33935f2e9118516581be30f9cf5
ALUMNO-G <ALUMNO-G@UCSP.AULAB>
2017-03-30 15:56:19 -0500

    Arrays

---
+++ MAYOR.c
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int c = 4;
+    cout << c ++ << endl;
+    cout << ++c << endl;
+
+    int a;
+    int b;
+    int z=(a>b)?a:b;
+    cout << "ingresa A : ";
+    cin >> a;
+    cout << "ingresa B : ";
+    cin >> b;
+    cout << "el mayor es :" <<z<<endl;
+
+
+
+
+
+
+    return 0;
+}
